eb90e8a5ab33c11cfe4e95cdfb36ad545058f9d3
Tuğberk Balıkçı <tugbalikci@gmail.com>
2023-02-24 19:32:46 +0300

    -Pipeline Folders Edits And ValidationBehaviour Creation

---
+++ src/Core/ESISA.Core.Application/Utilities/Pipelines/MediatR/Validation/FluentValidation/ValidationBehavior.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESISA.Core.Application.Utilities.Pipelines.MediatR.Validation.FluentValidation
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            ValidationContext<object> validationContext = new(request);
+
+            List<ValidationResult> validationResults = _validators.Select(validator => validator.Validate(validationContext)).ToList();
+
+            List<ValidationFailure> validationFailures = validationResults.SelectMany(validationResult  => validationResult.Errors)
+                                                                          .Where(failure=>failure != null)
+                                                                          .ToList();
+
+            this.CheckIfFailuresExists(validationFailures);
+
+            return next();
+        }
+
+        private void CheckIfFailuresExists(List<ValidationFailure> validationFailures)
+        {
+            if (validationFailures.Count != 0)
+                throw new ValidationException(validationFailures);
+        }
+    }
+}
