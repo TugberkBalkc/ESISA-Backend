8cb2a5321c72c5083b4475f971926707fadfba01
Tuğberk Balıkçı <tugbalikci@gmail.com>
2023-03-05 03:37:02 +0300

    - Creation Of BusinessRulesBase Class

--- src/Core/ESISA.Core.Application/Constants/Response/ResponseMessages.cs
+++ src/Core/ESISA.Core.Application/Constants/Response/ResponseMessages.cs
@@ -8,6 +8,9 @@ namespace ESISA.Core.Application.Constants.Response
 {
     public static class ResponseMessages
     {
+        public const String NotFound = "Not Found";
+        public const String AlreadyExists = "Already Exists";
+
         public const String ValidationError = "Validation Error";
     }
 }
