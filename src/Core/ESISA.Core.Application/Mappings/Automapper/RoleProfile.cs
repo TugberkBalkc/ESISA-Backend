41b7fe444349077e59161480aedd4ad0398abf15
Tuğberk Balıkçı <tugbalikci@gmail.com>
2023-03-12 05:51:22 +0300

    - Mapping Profile Update

--- src/Core/ESISA.Core.Application/Mappings/Automapper/RoleProfile.cs
+++ src/Core/ESISA.Core.Application/Mappings/Automapper/RoleProfile.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using ESISA.Core.Application.Dtos;
 using ESISA.Core.Application.Dtos.Security.Authorization;
+using ESISA.Core.Application.Features.MediatR.Commands.Roles.AddOperationClaim;
 using ESISA.Core.Application.Features.MediatR.Commands.Roles.Create;
 using ESISA.Core.Application.Features.MediatR.Commands.Roles.Update;
 using ESISA.Core.Domain.Entities;
@@ -23,6 +25,10 @@ namespace ESISA.Core.Application.Mappings.Automapper
              .ForMember(e => e.IsActive, e => e.MapFrom(e => e.RoleIsActive))
              .ForMember(e => e.Name, e => e.MapFrom(e => e.RoleName));
 
+            this.CreateMap<AddOperationClaimToRoleCommandRequest, RoleOperationClaim>()
+                .ForMember(e => e.RoleId, e => e.MapFrom(e => e.RoleId))
+                .ForMember(e => e.OperationClaimId, e => e.MapFrom(e => e.OperationClaimId));
+
             this.CreateMap<Role, RoleDto>()
                 .ForMember(e => e.RoleId, e => e.MapFrom(e => e.Id))
                 .ForMember(e => e.RoleCreatedDate, e => e.MapFrom(e => e.CreatedDate))
@@ -30,6 +36,15 @@ namespace ESISA.Core.Application.Mappings.Automapper
                 .ForMember(e => e.RoleIsActive, e => e.MapFrom(e => e.IsActive))
                 .ForMember(e => e.RoleIsDeleted, e => e.MapFrom(e => e.IsDeleted))
                 .ForMember(e => e.RoleName, e => e.MapFrom(e => e.Name));
+
+            this.CreateMap<RoleOperationClaim, RoleOperationClaimDto>()
+                .ForMember(e => e.RoleOperationClaimId, e => e.MapFrom(e => e.Id))
+                .ForMember(e => e.RoleOperationClaimCreatedDate, e => e.MapFrom(e => e.CreatedDate))
+                .ForMember(e => e.RoleOperationClaimModifiedDate, e => e.MapFrom(e => e.ModifiedDate))
+                .ForMember(e => e.RoleOperationClaimIsActive, e => e.MapFrom(e => e.IsActive))
+                .ForMember(e => e.RoleOperationClaimIsDeleted, e => e.MapFrom(e => e.IsDeleted))
+                .ForMember(e => e.RoleId, e => e.MapFrom(e => e.RoleId))
+                .ForMember(e => e.OperationClaimId, e => e.MapFrom(e => e.OperationClaim));
 
         }
     }
